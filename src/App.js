4f5a155a292d94b70e1fa1b40a7ad05419595a4a
Andre Jarboe <andrejarboe@gmail.com>
2023-01-09 20:30:31 -0500

    conditions for last and prev slide classes

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ function App() {
 					if (personIndex === index) {
 						position = 'activeSlide';
 					}
+					if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+						position = 'lastSlide';
+					}
 
 					return (
 						<article className={position} key={id}>
