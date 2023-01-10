b4c26467e8c0a0113ee96c12c89bdbd5617294af
Andre Jarboe <andrejarboe@gmail.com>
2023-01-10 14:25:20 -0500

    use effect for last index and 0 index

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
 	const [people, setPeople] = useState(data);
 	const [index, setIndex] = useState(0);
 
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		
+    if (index > lastIndex) {
+			setIndex(0);
+		}
+  }, [index, people]);
+  
+  //auto slide
+  
+
 	return (
 		<section className="section">
 			<div className="title">
